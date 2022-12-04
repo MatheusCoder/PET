5410baf6fe3f21226241d7fa328ddeeec8e89461
MatheusCoder <100002554+MatheusCoder@users.noreply.github.com>
2022-12-04 17:25:36 +0000

    Senha

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-par = 0
-impar = 0
-for i in range (5):
-    num = int(input('digite o número: '))
-    if num%2 == 0:
-        par = par+1
-    else:
-        impar = impar+1
-     
-print("os pares são:", par)
-print("os ímpares são:", impar)
+senha = int(input("digite a senha:"))
+while senha != 2002:
+    print("Senha invalida, digite novamente")
+    senha = int(input("digite a senha:"))
+    
+print("Acesso permitido")
