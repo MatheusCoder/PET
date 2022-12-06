a8ad76dfada89a24463ff20584846b9aa1caa62e
MatheusCoder <100002554+MatheusCoder@users.noreply.github.com>
2022-12-06 20:13:50 +0000

    Atividade pares

---
+++ Pares.py
@@ -0,0 +1,11 @@
+par = 0
+impar = 0
+for i in range (5):
+    num = int(input('digite o número: '))
+    if num%2 == 0:
+        par = par+1
+    else:
+        impar = impar+1
+
+print("os pares são:", par)
+print("os ímpares são:", impar)
