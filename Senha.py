368c1cb9d6041fc3f01b8a3814ee1a8dee5e8729
MatheusCoder <100002554+MatheusCoder@users.noreply.github.com>
2022-12-06 20:04:29 +0000

    Atividade senha fixa

---
+++ Senha.py
@@ -0,0 +1,10 @@
+senha = int(input("Digite a senha:"))
+while senha != 2002:
+    print("Senha invalida, digite novamente")
+    senha = int(input("Digite a senha:"))
+
+print("Acesso permitido")
+
+
+
+#https://stackoverflow.com/questions/8075877/converting-string-to-int-using-try-except-in-python
--- main.py
+++
@@ -1,6 +0,0 @@
-senha = int(input("digite a senha:"))
-while senha != 2002:
-    print("Senha invalida, digite novamente")
-    senha = int(input("digite a senha:"))
-    
-print("Acesso permitido")
