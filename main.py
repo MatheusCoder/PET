750a83f0b7bc682215def7badf1ed7cac9def165
MatheusCoder <100002554+MatheusCoder@users.noreply.github.com>
2022-12-13 23:45:26 -0300

    Criação da Conta Bonificada

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ print("2 - Consultar Saldo Conta")
 print("3 - Depositar na Conta")
 print("4 - Sacar na Conta")
 print("5 - Render Poupanca")
+print("6 - Render bônus")
+
 escolha = int(input("digite a opção desejada:"))
 while escolha > 0:
     if escolha == 1:
@@ -16,11 +18,14 @@ while escolha > 0:
         print("Criando Conta...")
         print("1 - Conta Corrente")
         print("2 - Conta Poupanca")
+        print("3 - Conta Bonificada")
         opcao = int(input("digite o tipo da conta:"))
         if opcao == 1:
             numConta = bancoUfrpe.criarConta()
-        else:
+        elif opcao == 2:
             numConta = bancoUfrpe.criarPoupanca()
+        elif opcao == 3:
+            numConta = banco.Ufrpe.criarBonificada()
         print("Conta criada:", numConta)
     elif escolha == 2:
         print("Consultando Saldo...")
@@ -51,4 +56,12 @@ while escolha > 0:
             print("Poupanca com novo saldo")
         else:
             print("A conta não é poupanca ou não existe")
+     elif escolha == 6:
+        print("Rendendo Bônus...")
+        numConta = int(input("digite o numero da conta bonificada:"))
+        resp = bancoUfrpe.renderbonificada(numConta)
+        if resp:
+            print("Bonificada com novo saldo")
+        else:
+            print("A conta não é bonificada ou não existe")        
     escolha = int(input("digite a opção desejada:"))
